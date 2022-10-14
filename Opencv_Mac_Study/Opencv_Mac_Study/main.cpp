7cdef77ccad2e99c4667967f190b534d9f635ea7
chengxiaoming <chengxiaoming@top-stack.com>
2022-10-14 14:29:33 +0800

    Opencv demo1

--- Opencv_Mac_Study/Opencv_Mac_Study/main.cpp
+++ Opencv_Mac_Study/Opencv_Mac_Study/main.cpp
@@ -10,12 +10,24 @@
 
 using namespace cv;
 
+void test1();
+void test2();
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    test1();
+    return 0;
+}
+
+void test1() {
     Mat img = imread("px1.jpg");
     namedWindow("Display", WINDOW_AUTOSIZE);
     imshow("img", img);
     
     waitKey(0);
-    return 0;
+}
+
+void test2() {
+    Mat m = Mat::eye(10, 10, CV_32FC1);
+    printf("Element is %f\n", m.at<float>(3,3));
 }
